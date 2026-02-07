c768e70f68fe77b571dc76c976931170337ef90a
DevNitrate <ambroiseinthewild@gmail.com>
2026-02-07 13:22:46 +0100

    quad follow camera and window size

--- assets/shader/frag.wgsl
+++ assets/shader/frag.wgsl
@@ -1,10 +1,16 @@
 #import bevy_pbr::forward_io::VertexOutput
 
-@group(3) @binding(1) var screen_texture: texture_2d<u32>;
+@group(3) @binding(1) var screen_texture: texture_2d<f32>;
 @group(3) @binding(2) var texture_sampler: sampler;
 
 @fragment
 fn fragment(in: VertexOutput) -> @location(0) vec4f {
-    return vec4f(textureSample(screen_texture, texture_sampler, in.uv)*-1);
+    let resolution = textureDimensions(screen_texture);
+    let aspect_ratio: f32 = f32(resolution.x) / f32(resolution.y);
+
+    var uv: vec2f = in.uv;
+    uv.x *= aspect_ratio;
+
+    return vec4f(textureSample(screen_texture, texture_sampler, in.uv));
     // return vec4f(in.uv, 0.0, 1.0);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{fs::File, io::BufReader};
 
-use bevy::{DefaultPlugins, app::{App, Startup}, asset::{Asset, Assets, Handle, RenderAssetUsages}, camera::Camera3d, camera_controller::free_camera::{FreeCamera, FreeCameraPlugin}, dev_tools::fps_overlay::FpsOverlayPlugin, ecs::system::{Commands, ResMut, Single}, image::Image, math::{Vec2, Vec3, primitives::Rectangle}, mesh::{Mesh, Mesh3d}, pbr::{Material, MaterialPlugin, MeshMaterial3d}, reflect::TypePath, render::render_resource::{AsBindGroup, Extent3d, TextureDimension, TextureFormat}, transform::components::Transform, window::{PresentMode, Window}};
+use bevy::{DefaultPlugins, app::{App, Startup, Update}, asset::{Asset, Assets, Handle, RenderAssetUsages}, camera::{Camera3d, OrthographicProjection, Projection, ScalingMode}, camera_controller::free_camera::{FreeCamera, FreeCameraPlugin}, dev_tools::fps_overlay::FpsOverlayPlugin, ecs::{message::MessageReader, system::{Commands, ResMut, Single}}, image::Image, math::{Vec2, Vec3, primitives::Rectangle}, mesh::{Mesh, Mesh3d}, pbr::{Material, MaterialPlugin, MeshMaterial3d}, reflect::TypePath, render::render_resource::{AsBindGroup, Extent3d, TextureDimension, TextureFormat}, transform::components::Transform, window::{PresentMode, Window, WindowResized}};
 use bytemuck::cast_slice;
 use tiff::{ColorType, decoder::{Decoder, DecodingResult}};
 
@@ -9,13 +9,12 @@ fn main() {
         .add_plugins((DefaultPlugins, MaterialPlugin::<ScreenMaterial>::default(), FreeCameraPlugin))
         .add_plugins(FpsOverlayPlugin::default())
         .add_systems(Startup, setup)
+        .add_systems(Update, resize_quad_system)
         .run();
 }
 
 fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<ScreenMaterial>>, mut win: Single<&mut Window>, mut images: ResMut<Assets<Image>>) {
     win.present_mode = PresentMode::AutoNoVsync;
-    commands.spawn((Camera3d::default(), FreeCamera::default(), Transform::from_xyz(100.0, 100.0, 100.0).looking_at(Vec3::ZERO, Vec3::Y)));
-
     let quad = meshes.add(Rectangle::from_size(Vec2 { x: win.width(), y: win.height() }));
     let img: Handle<Image> = images.add(load_tiff("assets/gebco.tif"));
     let material = materials.add(ScreenMaterial {
@@ -25,9 +24,41 @@ fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials
     });
 
     commands.spawn((
-        Mesh3d(quad),
-        MeshMaterial3d(material)
-    ));
+        Camera3d::default(),
+        FreeCamera {
+            walk_speed: 600.0,
+            ..Default::default()
+        },
+        Transform::from_xyz(0.0, 0.0, 0.0).looking_at(Vec3::NEG_Z, Vec3::Y),
+        Projection::from(
+            OrthographicProjection {
+                scale: 1.0,
+                scaling_mode: ScalingMode::WindowSize,
+                viewport_origin: Vec2::new(0.5, 0.5),
+                near: 0.0,
+                far: 1000.0,
+                area: Default::default()
+            }
+        ),
+    )).with_children(|parent| {
+        parent.spawn((
+            Mesh3d(quad),
+            MeshMaterial3d(material),
+            Transform::from_xyz(0.0, 0.0, -1.0)
+        ));
+    });
+}
+
+fn resize_quad_system(
+    mut resize_events: MessageReader<WindowResized>,
+    mut meshes: ResMut<Assets<Mesh>>,
+) {
+    for event in resize_events.read() {
+        let new_size = Vec2::new(event.width, event.height);
+        for mesh in meshes.iter_mut() {
+            *(mesh.1) = Rectangle::from_size(new_size).into();
+        }
+    }
 }
 
 fn load_tiff(path: &str) -> Image {
@@ -35,13 +66,39 @@ fn load_tiff(path: &str) -> Image {
     let mut decoder = Decoder::new(BufReader::new(file)).unwrap();
 
     let (width, height): (u32, u32) = decoder.dimensions().unwrap();
-    let mut buf = match decoder.colortype().unwrap() {
+    let data_i16 = compute_tiff("assets/gebco.tif");
+
+    let data_f32: Vec<f32> = data_i16.iter().map(|&v| (v as f32) / 10930.0).collect();
+
+    let image = Image::new(
+        Extent3d {
+            width,
+            height,
+            depth_or_array_layers: 1
+        },
+        TextureDimension::D2,
+        cast_slice(&data_f32).to_vec(),
+        TextureFormat::Rgba32Float,
+        RenderAssetUsages::all()
+    );
+
+    return image;
+}
+
+fn compute_tiff(path: &str) -> Vec<i16> {
+    let file: File = File::open(path).unwrap();
+    let mut decoder = Decoder::new(BufReader::new(file)).unwrap();
+
+    let (width, height): (u32, u32) = decoder.dimensions().unwrap();
+    let colortype: ColorType = decoder.colortype().unwrap();
+
+    let mut src_buf = match colortype {
         ColorType::Gray(_) =>  {
             println!("gray");
             DecodingResult::I16(vec![0; (width * height) as usize])
         },
         ColorType::RGB(_) => {
-            println!("rgba");
+            println!("rgb");
             DecodingResult::I16(vec![0; (width * height * 3) as usize])
         }
         ColorType::RGBA(_) => {
@@ -51,27 +108,35 @@ fn load_tiff(path: &str) -> Image {
         _ => panic!("unsupported tiff type")
     };
 
-    let _ = decoder.read_image_to_buffer(&mut buf).unwrap();
-    let data_i16: Vec<i16> = match buf {
+    let _ = decoder.read_image_to_buffer(&mut src_buf).unwrap();
+    let data_i16: Vec<i16> = match src_buf {
         DecodingResult::I16(data) => data,
         _ => panic!("Unexpected buffer type; expected I16"),
     };
 
-    let data_u32: Vec<u32> = data_i16.iter().map(|&v| v as u32).collect();
+    let mut buf: Vec<i16> = vec![0; (width * height * 4) as usize];
 
-    let image = Image::new(
-        Extent3d {
-            width,
-            height,
-            depth_or_array_layers: 1
+    match colortype {
+        ColorType::RGBA(_) => {
+            buf = data_i16
         },
-        TextureDimension::D2,
-        cast_slice(&data_u32).to_vec(),
-        TextureFormat::R32Sint,
-        RenderAssetUsages::all()
-    );
+        ColorType::Gray(_) => {
+            for y in 0..height as usize {
+                for x in 0..width as usize {
+                    let idx = y * width as usize + x;
+                    let pixel_val = data_i16[idx];
+                    let dst_idx = idx * 4;
+                    buf[dst_idx + 0] = pixel_val;
+                    buf[dst_idx + 1] = pixel_val;
+                    buf[dst_idx + 2] = pixel_val;
+                    buf[dst_idx + 3] = 32767; // max alpha
+                }
+            }
+        }
+        _ => unreachable!()
+    }
 
-    return image;
+    return buf;
 }
 
 #[derive(Asset, TypePath, AsBindGroup, Debug, Clone)]
